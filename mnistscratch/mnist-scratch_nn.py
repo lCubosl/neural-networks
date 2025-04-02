555572b32ceb6e8455cb8c52ca29fbf9ed8afa37
shifter <muislarques@gmail.com>
2025-04-02 02:04:56 +0100

    accuracy now at 85%

--- mnistscratch/mnist-scratch_nn.py
+++ mnistscratch/mnist-scratch_nn.py
@@ -3,7 +3,6 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 data = pd.read_csv("train.csv")
-data.head()
 # print(data)
 
 data = np.array(data)
@@ -21,6 +20,8 @@ X_train = data_train[1:n]
 X_train = X_train / 255.
 _, m_train = X_train.shape
 
+Y_train
+
 def init_params():
   W1 = np.random.rand(10, 784) - 0.5
   b1 = np.random.rand(10, 1) - 0.5
@@ -29,7 +30,7 @@ def init_params():
   return W1, b1, W2, b2
 
 def ReLU(Z):
-  return np.maximum(0,Z)
+  return np.maximum(Z, 0)
 
 def softmax(Z):
   A = np.exp(Z) / sum(np.exp(Z))
@@ -39,7 +40,7 @@ def forward_prop(W1, b1, W2, b2, x):
   Z1 = W1.dot(x) + b1
   A1 = ReLU(Z1)
   Z2 = W2.dot(A1) + b2
-  A2 = softmax(A1)
+  A2 = softmax(Z2)
   return Z1, A1, Z2, A2
 
 def deriv_ReLU(Z):
@@ -52,7 +53,6 @@ def one_hot(Y):
   return one_hot_Y
 
 def back_prop(Z1, A1, Z2, A2, W1, W2, X, Y):
-  m = Y.size
   one_hot_Y = one_hot(Y)
   dZ2 = A2 - one_hot_Y
   dW2 = 1 / m * dZ2.dot(A1.T)
@@ -87,4 +87,4 @@ def gradient_descent(X, Y, iterations, alpha):
       print("Accuracy: ", get_accuracy(get_predictions(A2), Y))
   return W1, b1, W2, b2
 
-W1, b1, W2, b2 = gradient_descent(X_train, Y_train, 500, 0.1)
+W1, b1, W2, b2 = gradient_descent(X_train, Y_train, 500, 0.10)
